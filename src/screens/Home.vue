7859ff505daddca90165cdfac392c7a586e2e477
dota <dota@Carlos-MacBook-Pro.local>
2018-02-04 13:35:38 +0800

    chatroom

--- src/screens/Home.vue
+++ src/screens/Home.vue
@@ -1,15 +1,18 @@
 <template>
-
+	<ChatRoom></ChatRoom>
 </template>
 
 <script>
+import ChatRoom from '../components/ChatRoom'
 export default {
   name: 'Home',
   created () {
     if (this.$route.path === '/') {
       this.$router.replace({path: '/chatroom'})
     }
+  },
+  components: {
+    ChatRoom
   }
 }
 </script>
-
