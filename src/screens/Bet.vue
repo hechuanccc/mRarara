2ef5aac603a5f11d0bbe11f36c0911a992d547e0
jerrywei <jerrywei17@unnotech.com>
2018-02-06 10:33:43 +0800

    fix style

--- src/screens/Bet.vue
+++ src/screens/Bet.vue
@@ -1,9 +1,17 @@
 <template>
-  <iframe :src="$store.state.systemConfig.mobileLotteryUrl" frameborder="0"></iframe>
+  <iframe class="results-frame" :src="$store.state.systemConfig.mobileLotteryUrl" frameborder="0"></iframe>
 </template>
 <script>
 export default {
   name: 'OnlineBet'
 }
 </script>
+
+<style lang="less" scoped>
+.results-frame {
+  height: 100%;
+  margin-bottom: -10px;
+  width: 100%;
+}
+</style>
 
